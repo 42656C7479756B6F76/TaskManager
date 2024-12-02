c5384b0e7c4a850d2de7f4f5b51c5cc6f8db1acf
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-12-02 16:04:53 +0300

    Migration: modified_at, deleted_at paste to task_comments

---
+++ src/TaskManager.Dal/Migrations/20240112143200_AddMonitorTimeToTaskComments.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace TaskManager.Dal.Migrations;
+
+[Migration(20240112143200, TransactionBehavior.None)]
+public class AddMonitorTimeToTaskComments : Migration
+{
+    public override void Up()
+    {
+        Alter.Table("task_comments")
+            .AddColumn("modified_at").AsDateTimeOffset().Nullable()
+            .AddColumn("deleted_at").AsDateTimeOffset().Nullable();
+    }
+
+    public override void Down()
+    {
+        Delete
+            .Column("modified_at")
+            .Column("deleted_at")
+            .FromTable("task_comments");
+    }
+}
