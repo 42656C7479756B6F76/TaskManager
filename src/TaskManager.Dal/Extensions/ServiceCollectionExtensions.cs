bf22b11231a69123c5e2a256165fe661a1bfc0b2
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-12-03 10:33:01 +0300

    DI repositories

--- src/TaskManager.Dal/Extensions/ServiceCollectionExtensions.cs
+++ src/TaskManager.Dal/Extensions/ServiceCollectionExtensions.cs
@@ -2,11 +2,36 @@ using TaskManager.Dal.Infrastructure;
 using TaskManager.Dal.Settings;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TaskManager.Dal.Repositories;
+using TaskManager.Dal.Repositories.Interfaces;
 
 namespace TaskManager.Dal.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddDalRepositories(
+        this IServiceCollection services)
+    {
+        AddPostgresRepositories(services);
+        AddRedisRepositories(services);
+        
+        return services;
+    }
+
+    private static void AddRedisRepositories(IServiceCollection services)
+    {
+        services.AddScoped<ITakenTaskRepository, TakenTaskRepository>();
+    }
+
+    private static void AddPostgresRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ITaskRepository, TaskRepository>();
+        services.AddScoped<ITaskLogRepository, TaskLogRepository>();
+        services.AddScoped<ITaskCommentRepository, TaskCommentRepository>();
+    }
+
+    
     public static IServiceCollection AddDalInfrastructure(
         this IServiceCollection services, 
         IConfigurationRoot config)
